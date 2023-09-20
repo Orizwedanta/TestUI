acf00444a2c343e74cf2467e993225221c5d085a
Orizwedanta <42068342+Orizwedanta@users.noreply.github.com>
2023-09-20 09:48:43 +0800

    Create mainActivity.java

---
+++ mainActivity.java
@@ -0,0 +1,23 @@
+package com.example.testui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
